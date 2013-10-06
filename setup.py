0d6e6ff6f69fdf9f28b16cbf557145d14fbcf76d
Honza Javorek <jan.javorek@gmail.com>
2013-10-06 13:34:21 +0200

    Library bootstrap.

---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+import re
+import sys
+import shlex
+import subprocess
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages  # NOQA
+
+
+base_path = os.path.dirname(__file__)
+
+
+# version
+meta_file = os.path.join(base_path, 'tipi/__init__.py')
+meta_file_contents = open(meta_file).read()
+meta = dict(re.findall(r'__([^_]+)__ = \'([^\']*)\'', meta_file_contents))
+
+
+# release a version, publish to GitHub and PyPI
+if sys.argv[-1] == 'publish':
+    command = lambda cmd: subprocess.check_call(shlex.split(cmd))
+    command('git tag v' + meta['version'])
+    command('git push --tags origin master:master')
+    command('python setup.py sdist upload')
+    sys.exit()
+
+
+setup(
+    name=meta['title'],
+    version=meta['version'],
+    description=(
+        'Converting plain ASCII punctuation characters into HTML '
+        'entities according to basic typography rules'
+    ),
+    long_description=open('README.rst').read(),
+    author=meta['author'],
+    author_email='mail@honzajavorek.cz',
+    url='https://github.com/honzajavorek/tipi',
+    license=open('LICENSE').read(),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+    ],
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup :: HTML',
+    )
+)
